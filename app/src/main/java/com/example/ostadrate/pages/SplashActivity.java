a8b8e3e382c6e3794f409a87570fa8bd66b1158c
G.Mrd <golnoosh.mrd78@gmail.com>
2019-08-26 20:43:02 +0430

    changed

--- app/src/main/java/com/example/ostadrate/pages/SplashActivity.java
+++ app/src/main/java/com/example/ostadrate/pages/SplashActivity.java
@@ -1,15 +1,14 @@
 package com.example.ostadrate.pages;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.ostadrate.R;
+import com.squareup.picasso.Picasso;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -20,47 +19,29 @@ public class SplashActivity extends AppCompatActivity {
 
         ImageView image = findViewById(R.id.image);
         ImageView image1 = findViewById(R.id.image1);
-        ImageView image2= findViewById(R.id.image2);
-        ImageView image3= findViewById(R.id.image3);
-        ImageView image4= findViewById(R.id.image4);
+        ImageView image2 = findViewById(R.id.image2);
+        ImageView image3 = findViewById(R.id.image3);
+        ImageView image4 = findViewById(R.id.image4);
+        ImageView image5 = findViewById(R.id.image5);
+        ImageView image6 = findViewById(R.id.image6);
+        ImageView image7 = findViewById(R.id.image7);
+        ImageView image8 = findViewById(R.id.image8);
 
+        Picasso.get().load(R.drawable.giphy2).into(image1);
+        Picasso.get().load(R.drawable.gophy2).into(image2);
+        Picasso.get().load(R.drawable.giphy2).into(image3);
+        Picasso.get().load(R.drawable.gophy2).into(image4);
+        Picasso.get().load(R.drawable.giphy2).into(image5);
+        Picasso.get().load(R.drawable.gophy2).into(image6);
+        Picasso.get().load(R.drawable.giphy2).into(image6);
+        Picasso.get().load(R.drawable.gophy2).into(image7);
+        Picasso.get().load(R.drawable.giphy2).into(image8);
 
-        ImageView image5= findViewById(R.id.image5);
-        ImageView image6= findViewById(R.id.image6);
-        ImageView image7= findViewById(R.id.image7);
-        ImageView image8= findViewById(R.id.image8);
-
-
-        GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(image1);
-        Glide.with(this).load(R.raw.giphy).into(imageViewTarget);
-
-        GlideDrawableImageViewTarget imageViewTarget1 = new GlideDrawableImageViewTarget(image2);
-        Glide.with(this).load(R.raw.gophy).into(imageViewTarget1);
-
-        GlideDrawableImageViewTarget imageViewTarget2 = new GlideDrawableImageViewTarget(image3);
-        Glide.with(this).load(R.raw.giphy2).into(imageViewTarget2);
-
-        GlideDrawableImageViewTarget imageViewTarget3 = new GlideDrawableImageViewTarget(image4);
-        Glide.with(this).load(R.raw.gophy2).into(imageViewTarget3);
-
-
-
-        GlideDrawableImageViewTarget imageViewTarget4 = new GlideDrawableImageViewTarget(image5);
-        Glide.with(this).load(R.raw.gophy2).into(imageViewTarget4);
-
-        GlideDrawableImageViewTarget imageViewTarget5 = new GlideDrawableImageViewTarget(image6);
-        Glide.with(this).load(R.raw.giphy2).into(imageViewTarget5);
-
-        GlideDrawableImageViewTarget imageViewTarget6 = new GlideDrawableImageViewTarget(image7);
-        Glide.with(this).load(R.raw.gophy2).into(imageViewTarget6);
-
-        GlideDrawableImageViewTarget imageViewTarget7 = new GlideDrawableImageViewTarget(image8);
-        Glide.with(this).load(R.raw.giphy2).into(imageViewTarget7);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                Intent intent = new Intent(SplashActivity.this, RateTeacherActivity.class);
                 startActivity(intent);
                 finish();
             }
